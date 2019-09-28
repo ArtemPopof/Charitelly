e5c0a6383647f858fb668af48574d2feb3215c0c
ArtemPopof <Deviling42@gmail.com>
2019-09-28 14:19:57 +0300

    extracted promo card as a component

--- src/components/PromoScroll.js
+++ src/components/PromoScroll.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { HorizontalScroll, Avatar } from "@vkontakte/vkui"
+import PromoScrollCard from './PromoScrollCard'
 
 
 export class PromoScroll extends Component {
@@ -7,43 +8,11 @@ export class PromoScroll extends Component {
         return (
             <HorizontalScroll>
                 <div style={{display: 'flex'}}>
-                    <div style={cardStyle}>
-                        <div style={headerLabel}>Header</div>
-                        <div style={text}>this is very important promo use should consider participate in it</div>
-                    </div>
-                    <div style={cardStyle}>
-                        some content
-                    </div>
+                    <PromoScrollCard/>
                 </div>
             </HorizontalScroll>
         )
     }
-}
-
-const cardStyle = {
-    width: '226px',
-    height: '80px',
-    margin: '0px 12px 0px 0px',
-    padding: '12px 12px 0px 16px',
-    backgroundColor: 'white',
-    borderRadius: '8px'
-}
-
-const headerLabel = {
-    fontFamily: 'SF UI Text',
-    fontSize: '15px',
-    lineHeight: '20px',
-    letterSpacing: '-0.32px',
-    color: '#F18627'
-}
-
-const text = {
-    margin: '4px 0px 0px 0px',
-    fontFamily: 'SF UI Text',
-    fontSize: '13px',
-    lineHeight: '16px',
-
-    color: '#909499'
 }
 
 export default PromoScroll
