4503af6044ba54f019afea2b51705c0afdef9c92
Alexander Ploskov <merhalak@gmail.com>
2019-09-29 01:30:05 +0300

    Add PrizePanel with Menu

--- src/panels/PrizePanel.js
+++ src/panels/PrizePanel.js
@@ -1,11 +1,25 @@
-import React from "react"
-import PrizeMenu from "../components/PrizeMenu";
+import React  from "react"
 import { Panel } from "@vkontakte/vkui";
+import Menu from "../components/Menu";
 
-const PrizePanel = ({ id }) => (
-    <Panel id={id}>
-        <PrizeMenu />
-    </Panel>
-);
+class PrizePanel extends React.Component {
+    PrizePanel(id) {
+        this.id = id;
+    }
+
+    itemSelected(item) {
+        console.log(item);
+    }
+
+    render() {
+        return (
+            <Panel id={this.id}>
+                <Menu
+                    menuItems={["Розыгрыши призов", "Подарки"]} 
+                    itemSelected={this.itemSelected} />
+            </Panel>
+        )
+    }
+}
 
 export default PrizePanel
