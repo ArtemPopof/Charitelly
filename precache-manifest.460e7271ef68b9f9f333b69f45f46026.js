65c8bc74f3ce2c8ec725a027886e8f356f54053f
ArtemPopof <Deviling42@gmail.com>
2019-09-29 11:35:54 +0300

    Updates

---
+++ precache-manifest.460e7271ef68b9f9f333b69f45f46026.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccf99f565d2aa8913fcd942d7f7ce6fd",
+    "url": "/Fundme/index.html"
+  },
+  {
+    "revision": "9d7f9508ce067e58c72e",
+    "url": "/Fundme/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "cd4dc93757731fb02356",
+    "url": "/Fundme/static/css/main.b4c54270.chunk.css"
+  },
+  {
+    "revision": "9d7f9508ce067e58c72e",
+    "url": "/Fundme/static/js/2.61433d60.chunk.js"
+  },
+  {
+    "revision": "cd4dc93757731fb02356",
+    "url": "/Fundme/static/js/main.de4f50b1.chunk.js"
+  },
+  {
+    "revision": "eb59a278835985f2a0df",
+    "url": "/Fundme/static/js/runtime-main.2662797c.js"
+  },
+  {
+    "revision": "432aa8843aff19ea55b55d7e9812f549",
+    "url": "/Fundme/static/media/SFUIDisplay-Regular.432aa884.ttf"
+  },
+  {
+    "revision": "47f0620f3790e38f621a9fbeb8eed066",
+    "url": "/Fundme/static/media/SFUIText-Bold.47f0620f.ttf"
+  },
+  {
+    "revision": "82c3bad5b014ba458378f24a3037be2a",
+    "url": "/Fundme/static/media/SFUIText-Medium.82c3bad5.ttf"
+  },
+  {
+    "revision": "0cfc29e3e69f8d0a6083fed65510eba4",
+    "url": "/Fundme/static/media/SFUIText-Regular.0cfc29e3.ttf"
+  },
+  {
+    "revision": "d051ab50042140753b6969b042edb7bc",
+    "url": "/Fundme/static/media/cat.d051ab50.jpg"
+  },
+  {
+    "revision": "5d3ce8f76ef7242aa248583cf9f2bd37",
+    "url": "/Fundme/static/media/golub.5d3ce8f7.jpg"
+  },
+  {
+    "revision": "cc8a569266ecf6449f06f3ec152ff87e",
+    "url": "/Fundme/static/media/man.cc8a5692.png"
+  },
+  {
+    "revision": "02806f9fcd3b1e5e083da49186e64640",
+    "url": "/Fundme/static/media/neva.02806f9f.jpg"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/Fundme/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "2493a3f5650b9ceb782dc4121b7f368b",
+    "url": "/Fundme/static/media/prize1.2493a3f5.png"
+  },
+  {
+    "revision": "3deaca6dccc48d7836f56a4897547cb3",
+    "url": "/Fundme/static/media/prize2.3deaca6d.png"
+  },
+  {
+    "revision": "939203a5bbea52085df4acfcfda1a4c8",
+    "url": "/Fundme/static/media/prize3.939203a5.png"
+  },
+  {
+    "revision": "b72283a9de300a2fe74b3c16c368bd41",
+    "url": "/Fundme/static/media/tigre.b72283a9.jpg"
+  }
+]);
